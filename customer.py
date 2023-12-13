6ad85cc4b26d693d8255fed7011a96b00bc665f8
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2023-12-13 19:33:53 +0100

    Fully functional customer for now

--- customer.py
+++ customer.py
@@ -4,31 +4,24 @@ from odoorpc import ODOO
 
 from models import Customer
 
-logging.basicConfig()
-# logger = logging.getLogger("odoorpc")
-logger = logging.getLogger("local")
-logger.setLevel(logging.DEBUG)
-
 
 class GetCustomer:
-    def fetch_customer_data(self, odoo: ODOO, partner_id: int) -> Customer:
-        partner_obj = odoo.env["res.partner"]
-        id = partner_id
+    def __init__(self, odoo: ODOO, partner_id: str):
+        self.odoo = odoo
+        self.partner_id = partner_id
+        self.resPartner = odoo.env["res.partner"]
+        self.customer_data = None
 
-        # Fetch customer data based on id
-        partner_id = partner_obj.search([("id", "=", id)], limit=1)[0]
-        if not partner_id:
-            # Partner not found
-            logging.error(f"Partner [{id}] not found in partner db.")
-            return Customer(id=-1, email="none@notfound.com")
+    def fetch_customer_data(self) -> Customer:
+        id = self.partner_id
+        partner_object = self.resPartner
 
-        # Partner found, process the partner data
-        logging.info(f"Proceeding to fetch customer data on {partner_id}.")
+        logging.info(f"Fetching customer data on ID:{id}.")
         fields_to_fetch = ["name", "email", "street", "city", "zip", "state_id", "country_id", "ref", "is_company"]
-        partner_details = partner_obj.read(partner_id, fields_to_fetch)[0]
+        partner_details = partner_object.read(id, fields_to_fetch)[0]
 
         customer_data = {
-            "id": partner_id,
+            "id": id,
             "email": partner_details.get("email", None),
             "name": partner_details.get("name", None),
             "street": partner_details.get("street", None),
@@ -39,6 +32,6 @@ class GetCustomer:
             "customer_reference": partner_details.get("ref", None),
             "is_company": partner_details.get("is_company", None),
         }
-        logging.debug(f"Returning customer data on customer [{partner_id}], {len(customer_data)} items")
 
-        return Customer(**customer_data)
+        logging.debug(f"Returning data on customer [{id}: {customer_data.name}]")
+        self.customer_data = Customer(**customer_data)
