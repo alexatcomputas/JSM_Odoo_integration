693486d5a52d2e2540a5684a5b1cb63ed861af20
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2024-01-02 11:40:56 +0100

    Product class not in use atm, commented out

--- odoo_function/product.py
+++ odoo_function/product.py
@@ -1,27 +1,23 @@
-from models import Product
+# from models import Product
 
 
-class ProductParser:
-    def __init__(self, customer_id):
-        self.customer_id = customer_id
-        self.product_data = self.fetch_product_data()
+# class ProductParser:
+#     def __init__(self, customer_id):
+#         self.customer_id = customer_id
+#         self.product_data = self.fetch_product_data()
 
-    def fetch_product_data(self) -> Product:
-        serial_numbers = self._get_serial_numbers(self.customer_id)
+#     def fetch_product_data(self) -> Product:
+#         serial_numbers = self._get_serial_numbers(self.customer_id)
 
-        product = Product(
-            
-        )
-        return
+#         product = Product()
+#         return
 
+#     def _get_serial_numbers(self) -> list[str]:
+#         return None
 
+#     # def _get_
 
-    def _get_serial_numbers(self) -> list[str]:
-        return None
-
-    def _get_
-
-    # name: Annotated[str, StringConstraints(strip_whitespace=True)]
-    # description: Annotated[str, StringConstraints(strip_whitespace=True)]
-    # serial_numbers: Optional[list[Annotated[str, StringConstraints(strip_whitespace=True)]]]
-    # list_price: Optional[float]
+#     # name: Annotated[str, StringConstraints(strip_whitespace=True)]
+#     # description: Annotated[str, StringConstraints(strip_whitespace=True)]
+#     # serial_numbers: Optional[list[Annotated[str, StringConstraints(strip_whitespace=True)]]]
+#     # list_price: Optional[float]
