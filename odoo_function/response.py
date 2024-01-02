d67554a60bd78cbd95b8acc7490c2765612910e7
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2024-01-02 12:31:18 +0100

    Return empty strings if no content in a return value

--- odoo_function/response.py
+++ odoo_function/response.py
@@ -33,37 +33,37 @@ def buildResponse(customer: GetCustomer = None, order: Order = None) -> response
         price_unit = order.so_line[0].price_unit
 
     data_fields = {
-        "sale_order__name": strip_invalid_chars(order.sale_order.name) if order.sale_order.name else None,
+        "sale_order__name": strip_invalid_chars(order.sale_order.name) if order.sale_order.name else "",
         "sale_order__sent_BGL": strip_invalid_chars(order.sale_order.sent_BGL) if order.sale_order.sent_BGL else "",
         "sale_order__sent_Flex": strip_invalid_chars(order.sale_order.sent_Flex) if order.sale_order.sent_Flex else "",
         "sale_order__sent_Hapro": strip_invalid_chars(order.sale_order.sent_Hapro) if order.sale_order.sent_Hapro else "",
-        "sale_order__customer_ref": strip_invalid_chars(order.sale_order.customer_ref) if order.sale_order.customer_ref else None,
-        "sale_order__tracking_no": strip_invalid_chars(order.sale_order.tracking_no) if order.sale_order.tracking_no else None,
+        "sale_order__customer_ref": strip_invalid_chars(order.sale_order.customer_ref) if order.sale_order.customer_ref else "",
+        "sale_order__tracking_no": strip_invalid_chars(order.sale_order.tracking_no) if order.sale_order.tracking_no else "",
         # sale_order__tracking_no_flex: strip_invalid_chars(order.sale_order.tracking_no_flex)
         # if order.sale_order.tracking_no_flex else None # In case it's needed after all
-        "res_partner__name": strip_invalid_chars(customer.name) if customer.name else None,
-        "res_partner__street": strip_invalid_chars(customer.street) if customer.street else None,
-        "res_partner__city": strip_invalid_chars(customer.city) if customer.city else None,
-        "res_partner__postal_code": strip_invalid_chars(customer.postal_code) if customer.postal_code else None,
-        "res_partner__state": strip_invalid_chars(customer.state[1]) if customer.state else None,
-        "res_partner__country": strip_invalid_chars(customer.country[1]) if customer.country else None,
+        "res_partner__name": strip_invalid_chars(customer.name) if customer.name else "",
+        "res_partner__street": strip_invalid_chars(customer.street) if customer.street else "",
+        "res_partner__city": strip_invalid_chars(customer.city) if customer.city else "",
+        "res_partner__postal_code": strip_invalid_chars(customer.postal_code) if customer.postal_code else "",
+        "res_partner__state": strip_invalid_chars(customer.state[1]) if customer.state else "",
+        "res_partner__country": strip_invalid_chars(customer.country[1]) if customer.country else "",
     }
 
     if isinstance(order.so_line, list):
-        data_fields["sale_order_line__name"] = strip_invalid_chars(name) if name else None
-        data_fields["sale_order_line__quantity"] = strip_invalid_chars(quantity) if quantity else None
-        data_fields["sale_order_line__price_unit"] = strip_invalid_chars(price_unit) if price_unit else None
+        data_fields["sale_order_line__name"] = strip_invalid_chars(name) if name else ""
+        data_fields["sale_order_line__quantity"] = strip_invalid_chars(quantity) if quantity else ""
+        data_fields["sale_order_line__price_unit"] = strip_invalid_chars(price_unit) if price_unit else ""
 
     # The normal instance, not a list
     else:
-        data_fields["sale_order_line__name"] = strip_invalid_chars(order.so_line.name) if order.so_line.name else None
+        data_fields["sale_order_line__name"] = strip_invalid_chars(order.so_line.name) if order.so_line.name else ""
         data_fields["sale_order_line__quantity"] = order.so_line.quantity
         data_fields["sale_order_line__price_unit"] = order.so_line.price_unit
 
     # Replace False with None for optional fields since they are returned as False from the Odoo db
     for key in data_fields:
         if data_fields[key] is False:
-            data_fields[key] = None
+            data_fields[key] = ""
 
     return responseModel(
         sale_order__name=data_fields["sale_order__name"],
