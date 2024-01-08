3b6bbb9cb6d521a1edfcbfe7df7f3df83300f4ab
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2024-01-08 22:26:07 +0100

    Omit Saleorderline from response if we fail to retrieve those datas

--- odoo_function/response.py
+++ odoo_function/response.py
@@ -35,7 +35,7 @@ def buildResponse(customer: GetCustomer = None, order: Order = None) -> Response
         # Using a list comprehension to extract names and 'join' to concatenate them
         return ", ".join(product.name for product in products)
 
-    if isinstance(order.so_line, list):
+    if order.so_line and isinstance(order.so_line, list):
         # I'm doing this in a rush please forgive me.
         # dash-concatenate each objects name attribute into one str (eg. "Name1 -  Name2 - Name3")
         name = " - ".join([obj.name for obj in order.so_line])
@@ -60,16 +60,21 @@ def buildResponse(customer: GetCustomer = None, order: Order = None) -> Response
         "product_product__name": strip_invalid_chars(concatenate_product_names(order.products)) if order.products else "",
     }
 
-    if isinstance(order.so_line, list):
+    if order.so_line and isinstance(order.so_line, list):
         data_fields["sale_order_line__name"] = strip_invalid_chars(name) if name else ""
         data_fields["sale_order_line__quantity"] = strip_invalid_chars(quantity) if quantity else ""
         data_fields["sale_order_line__price_unit"] = strip_invalid_chars(price_unit) if price_unit else ""
 
     # The normal instance, not a list
-    else:
+    elif order.so_line:
         data_fields["sale_order_line__name"] = strip_invalid_chars(order.so_line.name) if order.so_line.name else ""
         data_fields["sale_order_line__quantity"] = order.so_line.quantity
         data_fields["sale_order_line__price_unit"] = order.so_line.price_unit
+
+    else:
+        data_fields["sale_order_line__name"] = ""
+        data_fields["sale_order_line__quantity"] = 0
+        data_fields["sale_order_line__price_unit"] = 0
 
     # Replace False with None for optional fields since they are returned as False from the Odoo db
     for key in data_fields:
@@ -77,21 +82,21 @@ def buildResponse(customer: GetCustomer = None, order: Order = None) -> Response
             data_fields[key] = ""
 
     return ResponseModel(
-        sale_order__name=data_fields["sale_order__name"],
-        sale_order__sent_BGL=data_fields["sale_order__sent_BGL"],
-        sale_order__sent_Flex=data_fields["sale_order__sent_Flex"],
-        sale_order__sent_Hapro=data_fields["sale_order__sent_Hapro"],
-        sale_order__customer_ref=data_fields["sale_order__customer_ref"],
-        sale_order_line__name=data_fields["sale_order_line__name"],
-        sale_order_line__quantity=data_fields["sale_order_line__quantity"],
-        sale_order_line__price_unit=data_fields["sale_order_line__price_unit"],
-        sale_order__tracking_no=data_fields["sale_order__tracking_no"],
-        # sale_order__tracking_no_flex=optional_fields["sale_order__tracking_no_flex"],
-        res_partner__name=data_fields["res_partner__name"],
-        res_partner__street=data_fields["res_partner__street"],
-        res_partner__city=data_fields["res_partner__city"],
-        res_partner__postal_code=data_fields["res_partner__postal_code"],
-        res_partner__state=data_fields["res_partner__state"],
-        res_partner__country=data_fields["res_partner__country"],
-        product_product__name=data_fields["product_product__name"],
+        sale_order__name=data_fields.get("sale_order__name"),
+        sale_order__sent_BGL=data_fields.get("sale_order__sent_BGL"),
+        sale_order__sent_Flex=data_fields.get("sale_order__sent_Flex"),
+        sale_order__sent_Hapro=data_fields.get("sale_order__sent_Hapro"),
+        sale_order__customer_ref=data_fields.get("sale_order__customer_ref"),
+        sale_order_line__name=data_fields.get("sale_order_line__name"),
+        sale_order_line__quantity=data_fields.get("sale_order_line__quantity"),
+        sale_order_line__price_unit=data_fields.get("sale_order_line__price_unit"),
+        sale_order__tracking_no=data_fields.get("sale_order__tracking_no"),
+        # sale_order__tracking_no_flex=optional_fields.get("sale_order__tracking_no_flex"],
+        res_partner__name=data_fields.get("res_partner__name"),
+        res_partner__street=data_fields.get("res_partner__street"),
+        res_partner__city=data_fields.get("res_partner__city"),
+        res_partner__postal_code=data_fields.get("res_partner__postal_code"),
+        res_partner__state=data_fields.get("res_partner__state"),
+        res_partner__country=data_fields.get("res_partner__country"),
+        product_product__name=data_fields.get("product_product__name"),
     )
