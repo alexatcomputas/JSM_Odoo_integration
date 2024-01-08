2d6fa96497be56ad8f91b7103f11667c5a6ad46d
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2024-01-08 20:55:13 +0100

    Add product class. Used when retrievinbg product.product data (Product name)

--- odoo_function/product.py
+++ odoo_function/product.py
@@ -5,46 +5,6 @@ from odoorpc import ODOO
 from models import Product
 from odoo import Odoo as odoo
 
-# class ProductParser:
-#     def __init__(self, customer_id):
-#         self.customer_id = customer_id
-#         self.product_data = self.fetch_product_data()
-
-#     def fetch_product_data(self) -> Product:
-#         serial_numbers = self._get_serial_numbers(self.customer_id)
-
-#         product = Product()
-#         return
-
-#     def _get_serial_numbers(self) -> list[str]:
-#         return None
-
-#     # def _get_
-
-#     # name: Annotated[str, StringConstraints(strip_whitespace=True)]
-#     # description: Annotated[str, StringConstraints(strip_whitespace=True)]
-#     # serial_numbers: Optional[list[Annotated[str, StringConstraints(strip_whitespace=True)]]]
-#     # list_price: Optional[float]
-
-
-# Access the product model
-# product_model = odoo.env["product.product"]
-
-# Barcode retrieved from stock.move.line
-# barcode = "7090043790672"
-# barcode = "7090043790948"
-
-# Search for the product using the barcode
-# product_ids = product_model.search([("barcode", "=", barcode)])
-
-# Search for products
-# product_ids = Product.search([])
-# product_ids = [333]
-# product_ids = [290, 293]
-
-# Read the products' data
-# products = product_model.read(product_ids, ["name", "barcode"])
-
 
 class Products:
     def __init__(self, product_ids: list[int] = None, barcodes: list[str] = None, odoo: ODOO = odoo):
@@ -104,6 +64,3 @@ class Products:
             products.append(product)
 
         return products
-
-
-products = Products(product_ids=[260, 271, 263], barcodes=["7090043790672"])
