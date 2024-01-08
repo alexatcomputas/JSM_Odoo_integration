a1e0ec7d79386610b331ad299a05fb51b3b9beac
Alexander Eieland <alexander.kirkeby.eieland@computas.com>
2024-01-08 20:53:18 +0100

    Serialize and deserialize response for json

--- odoo_function/main.py
+++ odoo_function/main.py
@@ -1,7 +1,8 @@
+import json
 import logging
 
 import functions_framework
-from flask import Request
+from flask import Request, jsonify
 from werkzeug.exceptions import BadRequest
 
 from customer import GetCustomer
@@ -9,7 +10,7 @@ from environment import ENVIRONMENT
 from odoo import Odoo
 from orders import Order
 from response import buildResponse
-from util import dump_request
+from util import dump_request, process_and_jsonify_input
 
 if ENVIRONMENT == "local":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -17,10 +18,6 @@ if ENVIRONMENT == "local":
 else:
     logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
     logging.info("### Non-local environment detected ###")
-
-# from product import *
-
-# from traceability import *
 
 
 @functions_framework.http
@@ -35,17 +32,23 @@ def main(request: Request):
 
     except KeyError:
         logging.warning("Misconfigured request failed (Serial number field not found). 404")
-        return ("Request failed (Serial number field not found in request)", 404)
+        # return ("Request failed (Serial number field not found in request)", 404)
+        serialized_data = process_and_jsonify_input("Request failed (Serial number field not found in request)")
+        return serialized_data, 404
 
     except BadRequest as e:
         logging.error(f"Request failed (Bad request-> 400)\nRequest: {dump_request()}")
         logging.error(f"## Exception:##\n{e}")
-        return ("Invalid JSON", 400)
+        # return ("Invalid JSON", 400)
+        serialized_data = process_and_jsonify_input("Invalid JSON")
+        return serialized_data, 400
 
     except Exception as e:
         logging.error(f"Request failed -> 500:\nRequest: {dump_request()}")
         logging.error(f"## Exception:##\n{e}")
-        return ("Request failed (Unknown exception. Error logged). Returning 500", 500)
+        # return ("Request failed (Unknown exception. Error logged). Returning 500", 500)
+        serialized_data = process_and_jsonify_input("Request failed (Unknown exception. Error logged).")
+        return serialized_data, 500
 
     logging.info(f"Jira Service Management: Request received for SN:{serial_number}")
     logging.debug(f"Request json: {request.get_json(silent=False)}")
@@ -59,7 +62,9 @@ def main(request: Request):
 
     except ValueError as ve:
         logging.error("Returning ValueError")
-        return (f"Failed obtaining order data on serial number {order.serial_number}. Error: {ve}", 404)
+        # return (f"Failed obtaining order data on serial number {order.serial_number}. Error: {ve}", 404)
+        serialized_data = process_and_jsonify_input(f"Failed obtaining order data on serial number {order.serial_number}. Error: {ve}")
+        return serialized_data, 404
 
     except Exception as e:
         # Mandatory objects to populate for a meaningful order data response:
@@ -73,11 +78,13 @@ def main(request: Request):
             )
 
         logging.error(f"### Stack trace: ###\n{e}")
-        return (f"Failed obtaining order data on serial number {order.serial_number}. Exiting", 404)
+        serialized_data = process_and_jsonify_input(f"Failed obtaining order data on serial number {order.serial_number}")
+        return serialized_data, 404
 
     if not order.sale_order:
         logging.info("Serial number not found. Order data can't be built")
-        return ("Serial number not found", 404)
+        serialized_data = process_and_jsonify_input(f"Serial number not found: {serial_number}")
+        return serialized_data, 404
 
     # Get customer data #####
     try:
@@ -96,17 +103,19 @@ def main(request: Request):
 
     if order_OK and customer_OK:
         logging.info(f"Success, returning order [{order.sale_order.name}] and customer data to JSM")
-        orderResponse = buildResponse(customer, order).model_dump_json(by_alias=True, exclude_none=False)
+        # orderResponse = buildResponse(customer, order)
+        serialized_data = process_and_jsonify_input(buildResponse(customer, order))
 
-        return (orderResponse, 200)
+        return serialized_data, 200
 
     # Partial return
     elif order_OK:
         logging.info("Partial success, (Failed obtaining customer data). Only returning order data")
         logging.warning(f"Request: {dump_request()}")
-        orderResponse = buildResponse(customer=None, order=order).model_dump_json(by_alias=True, exclude_none=False)
+        # responseData = serialize_response(buildResponse(customer=None, order=order))
+        serialized_data = process_and_jsonify_input(buildResponse(customer=None, order=order))
 
-        return (orderResponse, 202)
+        return serialized_data, 202
 
     elif not (order_OK and customer_OK):
         return (f"SN Error:Failed retrieving order and customer data on {serial_number}", 400)
@@ -114,7 +123,10 @@ def main(request: Request):
     else:
         logging.error("Internal server error, 500")
         logging.error(f"Request: {dump_request()}")
-        return ("Internal server error", 500)
+
+        # responseData = serialize_response("Internal server error")
+        serialized_data = process_and_jsonify_input("Internal server error")
+        return serialized_data, 500
 
 
 if __name__ == "__main__":
